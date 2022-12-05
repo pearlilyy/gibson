e8da76a774b4ced05b83e80fc24a66e723d79a23
Pearl <flairpearllee@gmail.com>
2022-12-05 12:37:04 -0800

    upload sub css files

--- init.py
+++ init.py
@@ -8,6 +8,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 @app.route('/acc')
 def acc():
     return render_template('sub_acc.html')
--- static/js/header.js
+++ static/js/header.js
@@ -1,71 +1,88 @@
 //Gibson header.js
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     /*서브 메뉴*/
-    $('.main_menu').mouseenter(function(){
-        
-        $('.sub_menu').slideDown();
-        $('.sub_menu').mouseover(function(){
-            $(this).show();
+    $('.main_menu').mouseenter(function () {
+        $('.sub_menu').stop().slideDown();
+
+        $('.sub_menu li').hover(function () {
+            $(this).children('a').children('img').stop().fadeIn();
+        }, function () {
+            $(this).children('a').children('img').stop().fadeOut(200);
         });
-    });
-    $('.main_menu, .sub_menu').mouseleave(function(){
-        
+
+    }).mouseleave(function () {
+
         $('.sub_menu').hide();
+
     }); //.sub_menu hover
-    
-    
+
+
     /*언어 버튼 호버시 메뉴*/
-    $('.global').hover(function(){
-        
+    $('.global').hover(function () {
+
         $(this).children('a').children('img').stop().animate({
             rotate: '-270deg'
-        },300);
-        
+        }, 300);
+
         $(this).children('.language').slideDown();
-    }, function(){
+    }, function () {
         $(this).children('a').children('img').stop().animate({
             rotate: '0deg'
-        },300);
-        
+        }, 300);
+
         $(this).children('.language').hide();
     }); //.language 메뉴 보이기
-    
-    
+
+
     /*언어 버튼 클릭*/
-    $('.global>a').click(function(e){
+    $('.global>a').click(function (e) {
         e.preventDefault();
     }); //.global click
-    
-    
+
+    /*햄버거 버튼 호버*/
+    $('.hamburger').mouseenter(function () {
+        $('.hamburger span').addClass('move');
+
+    }).mouseleave(function () {
+        $('.hamburger span').removeClass('move');
+    }); //.hamburger hover
+
     /*햄버거 버튼 클릭*/
-    $('.hamburger').click(function(e){
-        
+    achg = false;
+    $('.hamburger').click(function (e) {
         e.preventDefault();
-        $('.sub_menu').stop().slideToggle();
-    }); //.hamburger click
-    
-    
-    /*돋보기 버튼 클릭*/
-    $('.magnifying').click(function(e){
-        
-        e.preventDefault();
-        
-    }); //magnifying click
-    
-    
-    
-    $(window).scroll(function(){
-        
-        var scTop = $(this).scrollTop();
-        
-        
-        
+
+        if (achg) {
+            achg = false;
+            $('.aside_in').animate({
+                right: '-300px'
+            });
+            $('.hamburger span').removeClass('on');
+        } else {
+            achg = true;
+            $('.aside_in').animate({
+                right: 0
+            });
+            $('.hamburger span').addClass('on');
+        }
     });
-    
-    
-    
-    
-    
+
+    /*돋보기 버튼 클릭*/
+    $('.magnifying').click(function (e) {
+
+        e.preventDefault();
+
+    }); //magnifying click
+
+    $(window).scroll(function () {
+        var scTop = $(this).scrollTop();
+    });
+
+    var aw = $('aside').offset().left;
+    var amw = $('.aside_in').offset().left;
+    console.log('aside ' + amw);
+
+
 }); //jQuery
